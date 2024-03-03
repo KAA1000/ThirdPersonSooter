ba3c04d8ae9d66ff8fe4ff2e3f9090bb943f1b0e
KAA1000 <kuzenko_1000@mail.ru>
2024-03-03 18:16:39 +0300

    Initial commit

---
+++ Assets/Player/Scripts/CameraRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float RotationSpeed = 1.0f; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + RotationSpeed * Input.GetAxis("Mouse X") * Time.deltaTime, 0);
+    }
+}
